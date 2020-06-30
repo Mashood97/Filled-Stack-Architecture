92195ccf1c2dce902003bbdc13c0493742b41717
Muhammad Mashood Siddiquie <40804440+Mashood97@users.noreply.github.com>
2020-06-30 18:58:32 +0500

    scratch file added for isolates

--- flutter_stack_api/scratch.dart
+++ flutter_stack_api/scratch.dart
@@ -1,31 +1,36 @@
-
-
+import 'dart:async';
 import 'dart:isolate';
+import 'package:http/http.dart' as http;
 
 main() {
-
   print('start');
-  Isolate.spawn(demo1,{
-    'name': 'Mashood',
-    'email': 'mashood@gmail.com'
-  });
-  Isolate.spawn(demo1,{
-    'name': 'Saad',
-    'email': 'Saad@gmail.com'
-  });
-  Isolate.spawn(demo1,{
-    'name': 'Saad',
-    'email': 'Saad@gmail.com'
-  });
-  Isolate.spawn(demo1,{
-    'name': 'Mashood',
-    'email': 'mashood@gmail.com'
-  });
+//  Isolate.spawn(demo1, {'name': 'Mashood', 'email': 'mashood@gmail.com'});
+//  Isolate.spawn(demo1, {'name': 'Saad', 'email': 'Saad@gmail.com'});
+//  Isolate.spawn(demo1, {'name': 'Saad', 'email': 'Saad@gmail.com'});
+//  Isolate.spawn(demo1, {'name': 'Mashood', 'email': 'mashood@gmail.com'});
 
-  print('end');
+
+
+  Timer(
+    Duration(milliseconds: 100),
+      ()=>Isolate.spawn(SignInUser, {'email': 'mashood@gmail.com','password':'Sodium97'}));
+  Isolate.spawn(SignInUser, {'email': 'mashood@gmail.com','password':'Sodium97'});
+
+      print('end');
 }
 
-demo1(Map<String,String> map){
+demo1(Map<String, String> map) {
   print('Hello ${map['name']}');
 }
 
+SignInUser(Map<String, dynamic> signInMap) async {
+  const String signIn = 'http://192.168.1.105:3000/adminAuth/signIn';
+  var result = await http.post(signIn, headers: {
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+  }, body: {
+    'userEmail': signInMap['email'],
+    'userPassword': signInMap['password'],
+  });
+  print(signInMap['email']);
+  print(result);
+}
