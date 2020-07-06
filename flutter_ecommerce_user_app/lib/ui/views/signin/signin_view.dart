4060bf4266fefa862be638644efbd796e8aaa64d
Muhammad Mashood Siddiquie <40804440+Mashood97@users.noreply.github.com>
2020-07-06 15:54:10 +0500

    signin_page to add

--- flutter_ecommerce_user_app/lib/ui/views/signin/signin_view.dart
+++ flutter_ecommerce_user_app/lib/ui/views/signin/signin_view.dart
@@ -7,8 +7,33 @@ class SignInView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<SignInViewModel>.reactive(
         builder: (ctx, child, model) => Scaffold(
+              body: SafeArea(
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
 
-        ),
+                           Image.asset(
+                            'assets/images/3.png',
+                            height: 200,
+                          ),
+
+                        Text(
+                          'Welcome Back!',
+                          style: Theme.of(context).textTheme.headline4.copyWith(
+                                fontSize: 24,
+                              ),
+                        ),
+
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
         viewModelBuilder: () => SignInViewModel());
   }
 }
+
+
